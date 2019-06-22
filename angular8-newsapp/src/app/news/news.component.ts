d05905c92ea3b497107545ffc39ca4e83dc2f007
afzaltkm <afzal.moideen@citysoft.net.au>
2019-06-22 14:24:08 +1000

    added html data

--- angular8-newsapp/src/app/news/news.component.ts
+++ angular8-newsapp/src/app/news/news.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-news',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
+  articles;
 
   ngOnInit() {
+    this.apiService.getNews().subscribe((data)=>{
+      console.log(data);
+      this.articles=data['articles'];
+    })
   }
 
 }
